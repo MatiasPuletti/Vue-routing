82855932d5c2d040a7d810005c11251a864c1c55
matiaspuletti <pulettimatias@gmail.com>
2021-08-24 13:53:03 +0200

    Implement redirect and catching all possible routes

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+  <h2>
+    Page not found! Why don't you try checking our
+    <router-link to="/teams">Teams</router-link>?
+  </h2>
+</template>
--- src/main.js
+++ src/main.js
@@ -5,13 +5,16 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
+    { path: '/', redirect: '/teams' },
+    { path: '/teams', component: TeamsList }, // With alias the url doesn't change, unlike with redirect
     { path: '/users', component: UsersList }, // our-domain.com/users => UsersList
-    { path: '/teams/:teamId', component: TeamMembers }
+    { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:notFound(.*)', component: NotFound } // Dynamic segment with a reg expression that means any character combination should lead to this route being loaded.
   ],
   linkActiveClass: 'active'
 });
